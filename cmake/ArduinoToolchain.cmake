1e94b3fb5d910671c7c849582b5be0e1fb8e0fcb
Anna Seppala <anna.seppaelae@tum.de>
2019-05-16 15:56:00 +0200

    now calculating size works even when cmake directory is not within the project.

--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -18,6 +18,7 @@ endif()
 # Add current directory to CMake Module path automatically
 if(EXISTS  ${CMAKE_CURRENT_LIST_DIR}/Platform/Arduino.cmake)
     set(CMAKE_MODULE_PATH  ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
+    set(CMAKE_ARDUINO_MODULE_PATH  ${CMAKE_CURRENT_LIST_DIR})
 endif()
 
 #=============================================================================#
